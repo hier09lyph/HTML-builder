7f16f676e0bb6cf24d6e3a964da1ba2e58eaaa8e
hier09lyph <v.varona89@gmail.com>
2023-05-04 11:19:52 +0200

    feat: complete 04-copy-directoty task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
 
+const fs = require("fs").promises;
+const path = require("path");
+
+const sourcePath = path.join(__dirname, "files");
+const destinationPath = path.join(__dirname, "files-copy");
+
+async function copyFolder() {
+  const destinationExists = await fs.stat(destinationPath)
+    .then(stats => stats.isDirectory())
+    .catch(() => console.log("Create folder 'files-copy'"));
+
+  if (destinationExists) {
+    await removeDir(destinationPath);
+  }
+
+  await fs.mkdir(destinationPath);
+
+  const files = await fs.readdir(sourcePath);
+  for (const file of files) {
+    const sourceFilePath = path.join(sourcePath, file);
+    const destinationFilePath = path.join(destinationPath, file);
+    await fs.copyFile(sourceFilePath, destinationFilePath);
+  }
+}
+
+copyFolder();
+
+async function removeDir(dirPath) {
+  const files = await fs.readdir(dirPath);
+  for (const file of files) {
+    const filePath = path.join(dirPath, file);
+    const fileStats = await fs.stat(filePath);
+    if (fileStats.isDirectory()) {
+      await removeDir(filePath);
+    } else {
+      await fs.unlink(filePath);
+    }
+  }
+
+  await fs.rmdir(dirPath);
+}
